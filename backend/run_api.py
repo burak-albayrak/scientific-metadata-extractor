06d42e978c0c614371cfd8aab581ba3779b89046
Burak Albayrak <ahmetburak.albayrak@mfatech.co>
2025-05-21 16:48:00 +0300

    Update backend port

--- backend/run_api.py
+++ backend/run_api.py
@@ -1,4 +1,6 @@
+import os
 from api import app
 
-if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
